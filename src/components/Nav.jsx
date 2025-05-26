d94df683706a6ec89821c58d3d2464c30e0147da
Christopher <christopher.ferraro34@gmail.com>
2025-05-26 08:52:04 -0400

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,6 +6,11 @@ export default function Nav() {
         <nav>
             <div className="nav-left">
                 <h1>Christopher Ferraro</h1>
+                <p>Full-Stack Web Developer</p>
+                <div className="nav-links">
+                    <a href="">GitHub</a>
+                    <a href="">LinkedIn</a>
+                </div>
             </div>
             <div className="nav-right">
                 <button>Controls</button>
